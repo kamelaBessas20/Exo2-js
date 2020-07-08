69faef5d058eb7447ebeb2be6da8a3f12b8da997
kamela <38164583+kamelaBessas20@users.noreply.github.com>
2020-07-08 14:05:06 +0200

    Delete Weapon.js

--- Weapon.js
+++
@@ -1,9 +0,0 @@
-class Weapon {
-    constructor(type, weaponName, damagePoints, length, weight) {
-        this.weaponName = weaponName;
-        this.damagePoints = damagePoints;
-        this.length = length;
-        this.weight = weight;
-        this.type = type;
-    }
-}
